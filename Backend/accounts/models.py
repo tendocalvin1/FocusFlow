a9e3852b4ce29677366a627b5ef9527a41838720
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-06-30 07:30:27 +0300

    feat: Implement user registration serializer and models for user management

--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -1,30 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 
 # Create your models here.
-class Register(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    email = models.EmailField()
-    password = models.CharField(max_length=15)
-    
-    
-    def __str__(self):
-        return f"{self.first_name} - {self.last_name}"
-    
-    
-class Login(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    
-    def __str__(self):
-        return f"{self.username}"
-    
-    
-class Logout(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    
-    def __str__(self):
-        return f"{self.username}"
-    
