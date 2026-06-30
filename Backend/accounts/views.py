7942e47d93ca8ea673a50c0c246c8f3f18863c34
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-06-30 09:11:44 +0300

    feat: Add register view with authentication for user registration

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
+from .serialisers import *
+from .models import *
+from datetime import date, timedelta
+from django.utils import timezone
 
 # Create your views here.
+@api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
+
+def register_view(request):
+    
