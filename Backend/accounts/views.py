0bd629d2beef5be2450de02119411812bad2a0ce
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-07-02 10:31:58 +0300

    feat: Update user views to include proper permission classes and enhance functionality

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
 from rest_framework.response import Response
 from .serialisers import *
@@ -8,6 +8,40 @@ from .serialisers import *
 
 # Create your views here.
 
-# register_view
+# Register a new user
 @api_view(["POST"])
+@permission_classes([AllowAny])
+def register_view(request):
+    if request.method == "POST":
+        serializer = RegisterSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# checking the profile of a user
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def profile_view(request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        
+# changing the password of the user
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def change_password_view(request):
+    serializer = ChangePasswordSerializer(request.user.set_password())
+    if serializer.is_valid():
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# A user logging out
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def logout_view(request):
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
