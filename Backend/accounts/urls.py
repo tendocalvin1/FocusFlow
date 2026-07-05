2c7961edba8ff6830d14e908d8be981f2fb7fa2d
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-07-05 20:28:54 +0300

    feat: Add API endpoints for user registration, profile, password change, logout, and login

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -2,5 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # register
+    path("api/auth/register/",view=views.register_view,name="register"),
     
+    # profile
+    path("api/profile/",view=views.profile_view,name="profile"),
+    
+    # Change password
+    path("api/auth/change-password/",view=views.change_password_view,name="change-password"),
+    
+    # logout
+    path("api/auth/logout/",view=views.logout_view,name="logout"),
+    
+    # login
+    path("api/token/",view=views.log_view,name="login"),
+    
+    # refresh
+    path("api/token/logout/",view=views.logout_view,name="refresh")
 ]
