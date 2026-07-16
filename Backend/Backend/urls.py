94b990557e394f3c7ca81f3fd9d23fffd42acdd2
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-07-16 07:20:37 +0300

    feat: Add drf-spectacular for API documentation and update URL patterns

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from rest_framework_simplejwt.views import TokenVerifyView
+from drf_spectacular.views import (SpectacularAPIView, SpectacularSwaggerView)
 
 # url patterns are used to show the different end points for the focusflow app
 # backend engineering using django
@@ -31,5 +32,7 @@ urlpatterns = [
     path("", include("accounts.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
