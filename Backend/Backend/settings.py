de2dee5c480aec99e47bb60087c1261af962bfeb
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-07-18 12:34:56 +0300

    feat: update database settings to use environment variables

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,11 +83,11 @@ WSGI_APPLICATION = "Backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "focusflow_db",
-        "USER": "focusflow_user",
-        "PASSWORD": "focusflow_password",
-        "HOST": "db",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
