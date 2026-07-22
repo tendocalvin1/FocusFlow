80d2099371e575088c05c479fe7d6ede901b574f
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-07-22 15:53:13 +0300

    feat: update docker-compose.yml to remove gunicorn command and enhance entrypoint.sh with improved gunicorn options

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -25,7 +25,8 @@ echo "Starting Gunicorn..."
 
 exec gunicorn Backend.wsgi:application \
     --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --timeout 120 \
-    --access-logfile - \
-    --error-logfile -
+    --workers=3 \
+    --timeout=120 \
+    --access-logfile=- \
+    --error-logfile=- \
+    --capture-output
