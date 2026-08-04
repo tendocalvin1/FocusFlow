d7dfb884f4f6b20b40ea1a34dd7e79e71a1e3761
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-08-04 17:07:42 +0300

    feat: add Test component for initial rendering; replace AppRoutes with a simple test layout

--- Frontend/focusflow-frontend/src/main.jsx
+++ Frontend/focusflow-frontend/src/main.jsx
@@ -1,12 +1,31 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// import "./index.css";
+
+// import AppRoutes from "./routes/AppRouter";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//     <React.StrictMode>
+//         <AppRoutes />
+//     </React.StrictMode>
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-import AppRoutes from "./routes/AppRouter";
+function Test() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>FocusFlow Test</h1>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <AppRoutes />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Test />
+  </React.StrictMode>
 );
