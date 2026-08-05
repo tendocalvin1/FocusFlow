35d26733a9552c585f5c8916d5e77f76d2948fb8
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-08-05 21:36:56 +0300

    feat: Add productivity model migrations

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -30,7 +30,7 @@ from Backend.views import home
 urlpatterns = [
     path("", home),
     path("admin/", admin.site.urls),
-    path("/api", include("productivity.urls")),
+    path("api/", include("productivity.urls")),
     path("", include("accounts.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
