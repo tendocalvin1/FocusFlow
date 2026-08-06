10364bc8c9a57101deae9aa9cfec921936983f75
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-08-06 20:34:55 +0300

    fix: update settings.py to import dj_database_url and improve dotenv loading with path checks

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -12,12 +12,25 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+
 from dotenv import load_dotenv
+print("DB_HOST =", os.getenv("DB_HOST"))
+print("DATABASE_URL =", os.getenv("DATABASE_URL"))
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")
+
+# load_dotenv(BASE_DIR / ".env")
+env_path = BASE_DIR / ".env"
+
+print("BASE_DIR =", BASE_DIR)
+print("ENV PATH =", env_path)
+print("ENV EXISTS =", env_path.exists())
+
+load_dotenv(env_path)
+
 
 
 # Quick-start development settings - unsuitable for production
