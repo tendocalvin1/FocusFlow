376fdd173c08338db1f2efe01ec81899eee00623
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-08-08 21:52:28 +0300

    fix: update API endpoint paths in urls.py and accounts/urls.py to streamline authentication routes

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path("", home),
     path("admin/", admin.site.urls),
     path("api/", include("productivity.urls")),
-    path("", include("accounts.urls")),
+    path("api/auth/", include("accounts.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -4,21 +4,21 @@ from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshVie
 
 urlpatterns = [
     # register
-    path("api/auth/register/",views.register_view,name="register"),
+    path("register/",views.register_view,name="register"),
 
     # profile   
-    path("api/profile/",views.profile_view,name="profile"),
+    path("profile/",views.profile_view,name="profile"),
     
     # change-password
-    path("api/auth/change-password/",views.change_password_view,name="change-password"),
+    path("change-password/",views.change_password_view,name="change-password"),
     
     # logout
-    path("api/auth/logout/",views.logout_view,name="logout"),
+    path("logout/",views.logout_view,name="logout")
 
-    # login
-    path("api/token/",TokenObtainPairView.as_view(),name="token_obtain_pair"),
+    # # login
+    # path("api/token/",TokenObtainPairView.as_view(),name="token_obtain_pair"),
 
     
-    # refresh
-    path("api/token/refresh/",TokenRefreshView.as_view(),name="token_refresh")
+    # # refresh
+    # path("api/token/refresh/",TokenRefreshView.as_view(),name="token_refresh")
 ]
