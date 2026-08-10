be3960282387b3c9be41d97d9f418bd15644d54d
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-08-10 17:26:02 +0300

    feat: integrate django-allauth for authentication and social login support

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("productivity.urls")),
     path("api/auth/", include("accounts.urls")),
+    path("accounts/", include("allauth.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
